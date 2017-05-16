a725b5bd215c1f6c31f124e6519941897414cca5
Robert Peri <robert.peri@ucc.on.ca>
2017-05-16 08:52:11 -0400

    Add files via upload

---
+++ info.php
@@ -0,0 +1,127 @@
+<?php
+
+  ?>
+  <html>
+
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>`
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <style>
+        body{
+           background-color: pink ;
+           border-style: dashed;
+          border-color: white;
+    }
+      h2{
+        text-align: center;
+        font-family:"Times New Roman", Times, serif;
+        font-size: 60px;
+        font-weight: bold;
+        position: relative;
+        bottom: 20px;
+      }
+      h1{
+        text-align: left;
+        font-family:"Times New Roman", Times, serif;
+        font-size: 30px;
+         position: relative;
+         left:120px;
+         font-weight: bold;
+      }
+      h3{
+        text-align: center;
+        font-family:"Times New Roman", Times, serif;
+        font-size: 20px;
+        position: relative;
+         left:80px;
+
+      }
+      h4{
+        text-align: left;
+        font-family:"Times New Roman", Times, serif;
+        font-size: 30px;
+         position: relative;
+         left:90px;
+         font-weight: bold;
+      </style>
+      <style>
+      .page-footer{
+         position: relative;
+      }
+    </style>
+
+
+    <script>
+    </script>
+
+    <body>
+      <h2>About Us</h2>
+      <h1>Robert Peri</h1>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      
+   <div class="fixed-action-btn horizontal click-to-toggle">
+    <a class="btn-floating btn-large red">
+      <i class="material-icons">menu</i>
+    </a>
+    <ul>
+
+      <li><a class="btn-floating pink accent-1" href="profile.php"><i class="material-icons">perm_identity</i></a></li>
+      <li><a class="btn-floating orange darken-1" href="everyone.php"><i class="material-icons">language</i></a></li>
+      <li><a class="btn-floating yellow darken-1" href="browse.php"><i class="material-icons">contact_phone</i></a></li>
+      <li><a class="btn-floating green" href="edit.php"><i class="material-icons">message</i></a></li>
+      <li><a class="btn-floating cyan lighten-2" href="settings.php"><i class="material-icons">live_help</i></a></li>
+      <li><a class="btn-floating deep-purple lighten-3" href="info.php"><i class="material-icons">info_outline</i></a></li>
+      <li><a class="btn-floating blue darken-3" href="logout.php"><i class="material-icons">settings_power</i></a></li>
+    </ul>
+  </div>
+  <div>
+           <div class="col s12 m7">
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="https://s24.postimg.org/ou52pwg79/9675910be434bae31494269507.png">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>Hi my name is Robert Peri, and I am the co-founder and co-onwer of New Twitter. I was born in </p>
+        </div>
+        <div class="card-action">
+        </div>
+      </div>
+    </div>
+  </div>
+</br>
+</br>
+    <h4>Andrew Downing</h4>
+
+   <div>
+      <div class="col s12 m7">
+    <div class="card horizontal">
+      <div class="card-image">
+      <img src="https://s2.postimg.org/3ydf2txa1/62259132ff97205d1494429689.png">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.An essay is, generally, a piece of writing that gives the author's own argument — but the definition is vague, overlapping with those of an article, a pamphlet, and a short story. Essays have traditionally been sub-classified as formal and informal</p>
+        </div>
+        <div class="card-action">
+        </div>
+      </div>
+    </div>
+  </div>
+              </span>
+               <body>
+               </body>
+            </div>
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
